c6a3106ac971dfe757c4e8ecfec7eb0e5ee6fe74
Culganosi <culgan_@hotmail.com>
2022-02-12 18:13:16 +0000

    added odd.js exercise

---
+++ odd.js
@@ -0,0 +1,19 @@
+const isOdd = function (num) {
+    return num % 2 === 1;
+}
+
+
+console.log(isOdd(10));
+console.log(isOdd(11));
+console.log(isOdd(12));
+console.log(isOdd(13));
+console.log(isOdd(14));
+console.log(isOdd(15));
+console.log(isOdd(16));
+console.log(isOdd(17));
+console.log(isOdd(18));
+console.log(isOdd(19));
+console.log(isOdd(20));
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
